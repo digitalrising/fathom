19694dfe3e11a037b03f4ce6c0bf67497d68717f
digitalrising <adigitalrising@gmail.com>
2018-04-02 22:27:19 -0400

    all of it

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x78, 0x86, 0x36, 0x05, 0x10, 0x4F, 0x7A, 0x1D, 0x48, 0x6A, 0x24, 0xF5, 0x90, 0x22, 0xAA, 0xB0 } };
+}
